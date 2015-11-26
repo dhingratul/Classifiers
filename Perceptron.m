2e8faefaef0fda44550ffd3b5f54b560f8b653bf
root <root@vostro>
2015-11-26 10:32:58 -0500

    Perceptron Shell

---
+++ Perceptron.m
@@ -0,0 +1,11 @@
+function [output]= Perceptron(tr,te,label)
+% ---------------------------------------------------------------------- %
+% OCR: Perceptron
+% Author: Atul Dhingra
+% dhingra[dot]atul92[at]gmail.com
+% Last Editted: 25 Nov 2015
+% ---------------------------------------------------------------------- %
+clear all;
+close all;
+
+end
