81f8bfdb19c666e503e04b6ab4ea1ac584186b53
dhingratul <dhingra.atul92@gmail.com>
2015-11-25 23:51:15 -0500

    Load Data into cells: for one digit, need to generalize it for all the rest

---
+++ Test.m
@@ -0,0 +1,12 @@
+% ---------------------------------------------------------------------- %
+% OCR
+% Author: Atul Dhingra
+% dhingra[dot]atul92[at]gmail.com
+% Last Editted: 25 Nov 2015
+% ---------------------------------------------------------------------- %
+clear all;
+close all;
+
+addpath('/home/dhingratul/Dropbox/Academics/Fall2015/CS-520/Assignment/Project/Dataset/data/digitdata');
+
+txt = ocr(testimages);
